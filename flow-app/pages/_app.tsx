47cc673b3e71ddfb4d5d8933972bc919986ad1bc
Anne Glines <anne.glines@dapperlabs.com>
2022-12-21 14:58:36 -0800

    add chakra

--- flow-app/pages/_app.tsx
+++ flow-app/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import { Provider } from '../components/app-provider';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Provider>
+        <Component {...pageProps} />
+      </Provider>
+    </ChakraProvider>
+  );
 }
