5195eb415392b5b18828089d520334c221221758
Anne Glines <anne.glines@dapperlabs.com>
2022-12-21 18:12:19 -0800

    force light mode

--- flow-app/pages/_app.tsx
+++ flow-app/pages/_app.tsx
@@ -1,10 +1,21 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import {
+  ChakraProvider,
+  extendTheme,
+  type ThemeConfig,
+} from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { Provider } from '../components/app-provider';
 
+const config: ThemeConfig = {
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config });
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Provider>
         <Component {...pageProps} />
       </Provider>
