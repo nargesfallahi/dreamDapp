a379a79b00f2bfb5c1dd01d967ef33e814a3b967
Anne Glines <anne.glines@dapperlabs.com>
2022-12-21 18:52:44 -0800

    logged in user info

---
+++ flow-app/components/user-info.tsx
@@ -0,0 +1,32 @@
+import { Avatar, Box, Button, Flex, Link } from '@chakra-ui/react';
+import { useAppProvider } from './app-provider';
+
+export const UserInfo = () => {
+  const { user } = useAppProvider();
+  return (
+    <Box
+      mt="20px"
+      h="400px"
+      boxShadow={'md'}
+      border="1px"
+      borderColor={'gray.200'}
+      borderRadius={5}
+      p={2}
+    >
+      <Flex flexDir={'column'} alignItems="center" pt={4}>
+        {user?.loggedIn ? (
+          <>
+            <Avatar size="md" name="User Name" />
+            <Link mt="10px" href="" color="twitter.400">
+              {user.addr}
+            </Link>
+          </>
+        ) : (
+          <Box>
+            <Button>Log in</Button>
+          </Box>
+        )}
+      </Flex>
+    </Box>
+  );
+};
